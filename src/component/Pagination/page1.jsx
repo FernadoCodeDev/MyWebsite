13b41ab24682dcc27c71e12a4baa96014f65f5aa
Fernando <fernando.e.mejia.e@gmail.com>
2025-04-02 10:26:33 -0600

    PAGE ORGANIZATION - PAGE NUMBER 1

---
+++ src/component/Pagination/page1.jsx
@@ -0,0 +1,103 @@
+import React from "react";
+import SVGhtml from "../../assets/svg/html";
+import SVGcss from "../../assets/svg/css";
+import SVGSass from "../../assets/svg/sass";
+import SVGJS from "../../assets/svg/js";
+import SVGTS from "../../assets/svg/ts";
+import SVGJquery from "../../assets/svg/Jquery";
+import SVGBootstrap from "../../assets/svg/Bootstrap";
+import SVGRedux from "../../assets/svg/Redux";
+import SVGReact from "../../assets/svg/svgreact";
+import SVGTailwind from "../../assets/svg/Tailwind";
+
+const page1 = () => {
+  return (
+    <div id="page-1" className="grid grid-cols-1 gap-4">
+      <h2 className="text-2xl font-bold md:text-4xl">Front-End</h2>
+
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-3">
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 bg-orange-300 rounded-full">
+            <SVGhtml />
+          </div>
+
+          <h3 className="font-bold">HTML</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 bg-indigo-300 rounded-full">
+            <SVGcss />
+          </div>
+
+          <h3 className="font-bold">CSS</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 bg-pink-300 rounded-full">
+            <SVGSass />
+          </div>
+
+          <h3 className="font-bold">SASS</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 bg-yellow-300 rounded-full">
+            <SVGJS />
+          </div>
+
+          <h3 className="font-bold">JavaScript</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 rounded-full bg-sky-300">
+            <SVGTS />
+          </div>
+
+          <h3 className="font-bold">Typescrip</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 rounded-full bg-cyan-300">
+            <SVGJquery />
+          </div>
+
+          <h3 className="font-bold">Jquery</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 rounded-full bg-violet-300">
+            <SVGBootstrap />
+          </div>
+
+          <h3 className="font-bold">Bootstrap</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 bg-purple-300 rounded-full">
+            <SVGRedux />
+          </div>
+
+          <h3 className="font-bold">Redux</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 rounded-full bg-sky-300">
+            <SVGReact />
+          </div>
+
+          <h3 className="font-bold">React</h3>
+        </div>
+
+        <div class="bg-slate-100 dark:bg-neutral-800 shadow-xl p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 rounded-full bg-cyan-300">
+            <SVGTailwind />
+          </div>
+
+          <h3 className="font-bold">Tailwind</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page1;
