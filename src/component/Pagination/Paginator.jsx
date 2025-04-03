d7f1e12304e6670c16ffbe29ebfeee1d63b8d767
Fernando <fernando.e.mejia.e@gmail.com>
2025-04-03 12:36:38 -0600

    Paginator Adjustment

--- src/component/Pagination/Paginator.jsx
+++ src/component/Pagination/Paginator.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 
 const sectionColors = [
-  "orange-600",
-  "sky-950",
-  "emerald-700",
-  "amber-400",
-  "slate-800",
-  "amber-900",
+  "bg-orange-600",
+  "bg-sky-950",
+  "bg-emerald-700",
+  "bg-amber-400",
+  "bg-slate-800",
+  "bg-amber-900",
 ];
 
 const Paginator = ({ pages }) => {
@@ -20,14 +20,14 @@ const Paginator = ({ pages }) => {
             key={index}
             className={
               ` border-2 border-gray-500 hover:border-gray-600 bg-transparent rounded-full w-12 h-12 relative cursor-pointer transition-all duration-700 ease-out ` +
-              (currentStep === index + 1 ? `border-${color}` : "border-gray-500")
+              (currentStep === index + 1 ? `${color} border-0` : "border-gray-500")
             }
             type="button"
             onClick={() => setCurrentStep(index + 1)}
           >
             <span
               className={`absolute top-1/2 left-1/2 w-8 h-8 rounded-full -translate-x-1/2 -translate-y-1/2 transition-all duration-700 ease-out ${
-                currentStep === index + 1 ? ` bg-${color} ` : "bg-gray-500 hover:bg-gray-600"
+                currentStep === index + 1 ? ` ${color} ` : "bg-gray-500 hover:bg-gray-600"
               }`}
             ></span>
           </button>
