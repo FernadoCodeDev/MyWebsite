0949a814324323cf9dccf6fff7f90817b281c5a0
Fernando <fernando.e.mejia.e@gmail.com>
2025-04-09 12:38:38 -0600

    Linkedin

--- src/component/layout/Header.jsx
+++ src/component/layout/Header.jsx
@@ -59,7 +59,7 @@ const Header = () => {
 
             <div className="grid items-center justify-center w-full max-w-[60rem] grid-cols-2 gap-4 md:grid-cols-4">
               <a
-                href="https://www.linkedin.com/in/fernando-mej%C3%ADa/"
+                href="https://www.linkedin.com/in/fernando-mejía"
                 target="_blank"
                 rel="noopener noreferrer"
               >
