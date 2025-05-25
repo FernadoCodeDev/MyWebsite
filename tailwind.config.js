3c207c2ad77b21f8920d3abbb5d66942245c6cf0
Fernando <fernando.e.mejia.e@gmail.com>
2025-05-25 13:33:44 -0600

    Beige Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,21 @@ module.exports = {
   darkMode: "class",
   theme: {
     extend: {
+      colors: {
+        beige: {
+          50: "#f3efe6",
+          100: "#ede7db",
+          200: "#e2d9c8",
+          300: "#d6cbb9",
+          400: "#cfc4b0",
+          500: "#c1b39e",
+          600: "#b9ad93",
+          700: "#a49580",
+          800: "#9c8e74",
+          900: "#8b7b67",
+          950: "#4b4236",
+        },
+      },
     },
   },
   plugins: [],
