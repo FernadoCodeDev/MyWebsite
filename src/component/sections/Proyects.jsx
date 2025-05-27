9fd2040b2b5e9b76ed35f773ba11c0c2daafef7d
Fernando <fernando.e.mejia.e@gmail.com>
2025-05-27 08:43:28 -0600

    Modal Projects

---
+++ src/component/sections/Proyects.jsx
@@ -0,0 +1,153 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import useScrollAnimation from "../hooks/ScrollAnimation";
+import GithubSVG from "../../assets/svg/Github2";
+import WebSVG from "../../assets/svg/web";
+import { ProjectsInfos } from "../ui/ProjectsModal";
+import ProjectsModal from "../ui/ProjectsModal";
+
+const Projects = () => {
+  const isVisible = useScrollAnimation("second-section");
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [SelectedProject, setSelectedProject] = useState(null);
+
+  const handleClick = (project) => {
+    setSelectedProject(project);
+    setIsOpen(true);
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [isOpen]);
+
+  return (
+   <div
+      className="w-full m-auto grid grid-cols-1 md:grid-cols-2 gap-4 max-w-[80rem] transition-all duration-1000 ease-in"
+     
+      id="Project"
+    >
+      <h1 className="text-2xl font-bold text-center md:text-4xl md:col-span-2">
+        Mis Proyectos
+      </h1>
+      <h3 className="text-xl font-bold text-center md:text-2xl md:col-span-2">
+        Creando, Aprendiendo y Evolucionando
+      </h3>
+
+      {ProjectsInfos.map((project) => (
+        <div
+          key={project.id}
+          onClick={() => handleClick(project)}
+          className={`relative rounded-2xl bg-slate-100 dark:bg-neutral-800 flex flex-col p-2`}
+        >
+          <div
+            className={`mt-10 h-auto rounded-l-2xl ${project.BackgroundDiv}  rounded-br-2xl rounded-bl-2xl relative flex flex-col`}
+          >
+            <img
+              src={project.logo}
+              alt={`${project.name} logo`}
+              className="absolute object-contain w-8 h-8 dark:invert -top-10 left-5"
+            />
+
+            {project.image1.includes(".mp4") ? (
+              <video
+                className="w-full h-auto p-4 rounded-2xl"
+                loop
+                autoPlay
+                muted
+                playsInline
+              >
+                <source src={project.image1} type="video/mp4" />
+              </video>
+            ) : (
+              <img
+                src={project.image1}
+                alt={`${project.name} main`}
+                className="w-full h-auto p-4 rounded-2xl"
+              />
+            )}
+
+            <div
+              className={`w-1/3 h-11 ${project.BackgroundDiv} z-10 rounded-2xl absolute transform -top-7 right-5 skew-x-[-40deg]`}
+            ></div>
+            <div
+              className={`w-1/4 h-11 ${project.BackgroundDiv} z-10 rounded-tr-2xl absolute transform -top-7 right-0 `}
+            >
+              <div className="absolute flex flex-row justify-around w-full h-12 p-1">
+                {project.web && (
+                  <a
+                    href={project.web}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-8 h-8"
+                  >
+                    <WebSVG />
+                  </a>
+                )}
+                {project.github && (
+                  <a
+                    href={project.github}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="w-8 h-8"
+                  >
+                    <GithubSVG />
+                  </a>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-col items-center justify-center flex-1 p-2 mt-2">
+            <h1 className="text-2xl font-extrabold text-center">
+              {project.name}
+            </h1>
+
+            <div className="flex flex-col justify-center flex-1">
+              <div className="text-center">
+                <h1 className="text-base md:text-xl">{project.Paragraph}</h1>
+                {project.Paragraph2 && (
+                  <h2 className="mt-2 text-sm md:text-base">
+                    {project.Paragraph2}
+                  </h2>
+                )}
+              </div>
+
+              <div className="w-full h-[1px] bg-black dark:bg-white my-4"></div>
+
+              <div className="grid items-center justify-center grid-cols-4 gap-4 mx-auto">
+                {project.icons.map((item, index) => (
+                  <div
+                    key={index}
+                    className={`flex flex-col items-center justify-center w-12 h-12 p-2 ${item.Background} rounded-full md:w-20 md:h-20 md:p-4`}
+                  >
+                    {item.isImage ? (
+                      <img
+                        src={item.icon}
+                        alt="Icono"
+                        className="object-contain w-8 h-8 md:w-20 md:h-20 invert"
+                      />
+                    ) : (
+                      item.icon
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+      <ProjectsModal
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+        Project={SelectedProject}
+      />
+    </div>
+  );
+};
+
+export default Projects;
---
+++ src/component/ui/ProjectsModal.jsx
@@ -0,0 +1,223 @@
+import { useEffect, useState } from "react";
+import GithubSVG from "../../assets/svg/Github2";
+import WebSVG from "../../assets/svg/web";
+import Close from "../../assets/svg/Close";
+import SVGRect from "../../assets/svg/svgreact";
+import SVGTailwind from "../../assets/svg/Tailwind";
+import SVGresponsive from "../../assets/svg/responsive";
+import SVGJS from "../../assets/svg/js";
+import SVGSass from "../../assets/svg/sass";
+import SVGphp from "../../assets/svg/php";
+import SVGmysql from "../../assets/svg/mysql";
+import MVC from "../../assets/img/MVC.webp";
+import Clerk from "../../assets/img/Clerk.webp";
+
+import CodiGolLogo from "../../assets/img/CódiGolLogo2.webp";
+import CodiGol from "../../assets/img/CodiGolLogo.webp";
+import CodiGol2 from "../../assets/img/CodiGol2.webp";
+import CodiGol3 from "../../assets/img/CodiGol3.webp";
+import CodiGol4 from "../../assets/video/CódiGolClip.mp4";
+
+import LandingMine from "../../assets/img/LandingMine.webp";
+import LandingMineVideo from "../../assets/video/LandingMineVideo.mp4";
+
+import CosmicPizza from "../../assets/img/CosmicPizza.webp";
+import CosmicPizzaB from "../../assets/img/CosmicPizzaB.webp";
+import CosmicPizza2 from "../../assets/img/CosmicPizza2.webp";
+import CosmicPizza3 from "../../assets/img/CosmicPizza3.webp";
+import CosmicPizza4 from "../../assets/img/CosmicPizza4.webp";
+
+import KeysHomes from "../../assets/img/KeysHomes.webp";
+import KeysHomesB from "../../assets/img/KeysHomesB.webp";
+import KeysHomes2 from "../../assets/img/KeysHomes2.webp";
+import KeysHomes3 from "../../assets/img/KeysHomes3.webp";
+import KeysHomes4 from "../../assets/img/KeysHomes4.webp";
+
+export const ProjectsInfos = [
+  {
+    id: 1,
+    name: "CódiGol",
+    Paragraph:
+      "Pequeño videojuego web diseñado para ayudarte a aprender y practicar Tailwind CSS de forma divertida y diferente. Inspirado en la emoción del fútbol, este juego te reta a anotar goles completando desafíos de diseño utilizando Tailwind.",
+    Paragraph2:
+      "Proyecto desarrollado y destacado en la Hackathon de MiduDev 2025, Donde se integro Clerk SDK para la autenticación de usuarios",
+    BackgroundDiv: "bg-neutral-900",
+    web: "https://codigol-demo.netlify.app/",
+    github: "https://github.com/FernadoCodeDev/CodiGol-Demo",
+    logo: CodiGolLogo,
+    image1: CodiGol,
+    image2: CodiGol2,
+    image3: CodiGol3,
+    image4: CodiGol4,
+    icons: [
+      { icon: <SVGJS />, Background: "bg-yellow-300", isImage: false },
+      { icon: <SVGRect />, Background: "bg-sky-300", isImage: false },
+      { icon: <SVGTailwind />, Background: "bg-cyan-300", isImage: false },
+      { icon: Clerk, Background: "bg-purple-300", isImage: true },
+    ],
+  },
+  {
+    id: 2,
+    name: "Landing Mine",
+    Paragraph:
+      "Colección de landing pages con diversas temáticas, diseñadas para crear interfaces modernas, atractivas y altamente eficientes. con un total de 19 landing Pages.",
+    Paragraph2: "",
+    web: "https://landingmine.netlify.app/",
+    github: "https://github.com/FernadoCodeDev/LandingMine",
+    logo: LandingMine,
+    image1: LandingMineVideo,
+    BackgroundDiv: "bg-blue-950",
+    icons: [
+      { icon: <SVGJS />, Background: "bg-yellow-300", isImage: false },
+      { icon: <SVGRect />, Background: "bg-sky-300", isImage: false },
+      { icon: <SVGTailwind />, Background: "bg-cyan-300", isImage: false },
+      { icon: <SVGresponsive />, Background: "bg-sky-300", isImage: false },
+    ],
+  },
+  {
+    id: 3,
+    name: "Cosmic Pizza",
+    Paragraph:
+      "Plataforma web diseñada para una pizzería, integrando un sistema de reservaciones que mejora la experiencia del usuario y la gestión del negocio. Un proyecto que combina diseño intuitivo con funcionalidad práctica.",
+    Paragraph2: "",
+    github: "https://github.com/FernadoCodeDev/Cosmic-Pizza",
+    logo: CosmicPizza,
+    image1: CosmicPizzaB,
+    image2: CosmicPizza2,
+    image3: CosmicPizza3,
+    image4: CosmicPizza4,
+    BackgroundDiv: "bg-[#090f1f]",
+    icons: [
+      { icon: <SVGJS />, Background: "bg-yellow-300", isImage: false },
+      { icon: <SVGphp />, Background: "bg-indigo-300", isImage: false },
+      { icon: <SVGmysql />, Background: "bg-orange-300", isImage: false },
+      { icon: MVC, Background: "bg-slate-800", isImage: true },
+    ],
+  },
+  {
+    id: 4,
+    name: "Keys Homes",
+    Paragraph:
+      "Plataforma web enfocada en bienes raíces, diseñada para ofrecer una experiencia fluida y organizada en la búsqueda y gestión de propiedades. Un desafío donde la estructura y la optimización han sido clave para su desarrollo.",
+    Paragraph2: "",
+    BackgroundDiv: "bg-[#0f2925] ",
+    web: "http://keyshome.atwebpages.com/",
+    github: "https://github.com/FernadoCodeDev/KeysHomes",
+    logo: KeysHomes,
+    image1: KeysHomesB,
+    image2: KeysHomes2,
+    image3: KeysHomes3,
+    image4: KeysHomes4,
+    icons: [
+      { icon: <SVGJS />, Background: "bg-yellow-300", isImage: false },
+      { icon: <SVGSass />, Background: "bg-pink-300", isImage: false },
+      { icon: <SVGphp />, Background: "bg-indigo-300", isImage: false },
+      { icon: <SVGmysql />, Background: "bg-orange-300", isImage: false },
+    ],
+  },
+];
+export default function ProjectsModal({ isOpen, onClose, Project }) {
+  if (!isOpen || !Project) return null;
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const mediaItems = [
+    Project.image1,
+    Project.image2,
+    Project.image3,
+    Project.image4,
+  ].filter(Boolean);
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => (prev === 0 ? mediaItems.length - 1 : prev - 1));
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => (prev === mediaItems.length - 1 ? 0 : prev + 1));
+  };
+
+  const isVideo = (src) => {
+    return typeof src === "string" && src.match(/\.(mp4|webm|ogg)$/);
+  };
+
+  const currentMedia = mediaItems[currentIndex];
+
+  return (
+    <div className="fixed inset-0 z-50 flex items-center justify-center text-white bg-black backdrop-blur-[4px] bg-opacity-60">
+      <div className="relative p-2 m-auto overflow-hidden max-w-[70rem] rounded-2xl bg-black">
+        <button
+          onClick={onClose}
+          className="absolute z-20 w-16 h-auto top-4 right-4"
+        >
+          <Close />
+        </button>
+
+        <div className="relative z-20 grid grid-cols-1 gap-4 p-4 mt-16">
+          <div className="flex items-center justify-center w-full h-auto bg-black ">
+            {isVideo(currentMedia) ? (
+              <video
+                src={currentMedia}
+                controls
+                className="w-full h-auto rounded-xl"
+              />
+            ) : (
+              <img
+                src={currentMedia}
+                alt="preview"
+                className="w-full h-auto rounded-xl"
+              />
+            )}
+
+            <button
+              onClick={handlePrev}
+              className="absolute px-3 py-1 text-xl text-white bg-black bg-opacity-50 rounded-full left-4"
+            >
+              ‹
+            </button>
+            <button
+              onClick={handleNext}
+              className="absolute px-3 py-1 text-xl text-white bg-black bg-opacity-50 rounded-full right-4"
+            >
+              ›
+            </button>
+          </div>
+
+          <div className="grid grid-cols-1 gap-4 p-4 mt-6 ">
+            <div className="grid items-center grid-cols-1 gap-4">
+              <div className="grid items-center w-full grid-cols-4 gap-2 mx-auto">
+                {Project.icons?.map((item, i) => (
+                  <div
+                    key={i}
+                    className={`flex flex-col items-center justify-center w-12 h-12 p-2 ${item.Background} rounded-full md:w-20 md:h-20 md:p-4`}
+                  >
+                    {item.isImage ? (
+                      <img
+                        src={item.icon}
+                        alt="Icono"
+                        className="object-contain w-8 h-8 md:w-20 md:h-20 invert"
+                      />
+                    ) : (
+                      item.icon
+                    )}
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            <div className="grid items-center grid-cols-1 gap-4">
+              <h1 className="text-xl font-bold md:text-2xl">
+                {Project.name}
+              </h1>
+              <p className="text-base md:text-xl m-auto max-w-[40rem]">
+                {Project.Paragraph}
+              </p>
+              <p className="text-base md:text-xl m-auto max-w-[40rem]">
+                {Project.Paragraph2}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
