85fd109dcbba0a1b10c45e7b1ce3a479f7e80c66
Fernando <fernando.e.mejia.e@gmail.com>
2025-05-27 10:16:17 -0600

    Adjustment Grid Pagination

--- src/component/Pagination/page5.jsx
+++ src/component/Pagination/page5.jsx
@@ -14,7 +14,7 @@ const page5 = () => {
         Herramientas
       </h2>
 
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
         <div class="h-auto bg-slate-100 dark:bg-neutral-800 shadow-xl dark:shadow-neutral-700/40 p-4 text-center flex flex-col justify-center items-center gap-4">
           <div className="flex flex-col items-center justify-center w-20 h-20 p-3 bg-orange-300 rounded-full">
             <SVGgit />
