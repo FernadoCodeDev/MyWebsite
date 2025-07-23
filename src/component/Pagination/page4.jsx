1e105cc7dd08e0c92995c55be3f77217af229118
Fernando <fernando.e.mejia.e@gmail.com>
2025-07-23 12:17:44 -0600

    WordPress

--- src/component/Pagination/page4.jsx
+++ src/component/Pagination/page4.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SVGdocker from "../../assets/svg/docker";
+import SVGWordPress from "../../assets/svg/wordpressSVG";
 
 const page4 = () => {
   return (
@@ -9,6 +10,12 @@ const page4 = () => {
       </h2>
 
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+        <div class="h-auto bg-slate-100 dark:bg-neutral-800 shadow-xl dark:shadow-neutral-700/40 p-4 text-center flex flex-col justify-center items-center gap-4">
+          <div className="flex flex-col items-center justify-center w-20 h-20 p-3 rounded-full bg-sky-700">
+            <SVGWordPress />
+          </div>
+          <h1 className="font-bold">WordPress</h1>
+        </div>
 
         <div class="h-auto bg-slate-100 dark:bg-neutral-800 shadow-xl dark:shadow-neutral-700/40 p-4 text-center flex flex-col justify-center items-center gap-4">
           <div className="flex flex-col items-center justify-center w-20 h-20 p-3 bg-blue-300 rounded-full">
