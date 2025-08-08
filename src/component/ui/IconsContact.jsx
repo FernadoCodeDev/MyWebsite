350e399d42b51c21ffb7f845da891cc816afe56b
Fernando <fernando.e.mejia.e@gmail.com>
2025-08-08 12:38:12 -0600

    Header astro

--- src/component/ui/IconsContact.jsx
+++ src/component/ui/IconsContact.jsx
@@ -1,32 +1,11 @@
 import React, { useState } from "react";
-import DarkMode from "../component/ui/DarkMode";
-import Linkedin from "../assets/svg/Linkedin";
-import GitHub from "../assets/svg/GitHub";
-import Cv from "../assets/svg/CV";
-import Gmail from "../assets/svg/Gmail"
+import Linkedin from "../../assets/svg/Linkedin";
+import GitHub from "../../assets/svg/GitHub";
+import Cv from "../../assets/svg/CV";
+import Gmail from "../../assets/svg/Gmail";
 
-const Header = () => {
+const IconsContact = () => {
   return (
-    <div className="">
-      <div className="flex flex-col items-start justify-between w-full h-auto gap-4 p-4 pb-4 border-b-2 border-gray-400 align-center">
-        <div className="flex flex-row items-center w-full justify-between max-w-[80rem] m-auto gap-4">
-          <div className="flex flex-row items-center justify-center gap-2">
-            <img src="/img/ImgFirstSection.webp" alt="ImgFirstSection" className="w-16 h-auto" />
-
-            <h1 className="text-2xl font-bold text-blue-500">Fernando Mejía</h1>
-
-          </div>
-
-          <DarkMode />
-        </div>
-        <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut expedita quos maxime explicabo doloribus exercitationem, quibusdam tempora. Natus dolor rem sit quia tenetur! Officia tempore similique enim voluptate quasi esse?</p>
-        <div className="flex flex-row gap-2" >
-          <GitHub />
-          <p className="font-bold text-blue-400">fercode.netlify.app/</p>
-
-        </div>
-
-
         <div className="flex flex-row gap-4 items-start justify-between max-w-[60rem]">
 
           <a href=""
@@ -75,12 +54,7 @@ const Header = () => {
 
 
         </div>
-
-
-
-      </div>
-    </div>
   );
 };
 
-export default Header;
+export default IconsContact;
