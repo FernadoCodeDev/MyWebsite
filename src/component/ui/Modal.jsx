114b3c648474c3a4770c850ea4027fcf6955023a
Fernando <fernando.e.mejia.e@gmail.com>
2025-08-10 11:19:36 -0600

    SVG Astro

--- src/component/ui/Modal.jsx
+++ src/component/ui/Modal.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import CVES from "../../assets/cv/CV - Fernando Mejía Español.pdf";
 import CVEN from "../../assets/cv/CV - Fernando Mejía English.pdf";
-import Cv from "../../assets/svg/CV";
+import CV from "../../assets/svg/CV";
 
 const DownloadCVButton = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -23,14 +23,14 @@ const DownloadCVButton = () => {
       className="flex flex-row gap-2 p-2 transition-all duration-700 ease-out bg-transparent rounded-lg cursor-pointer cursor hover:bg-slate-300 dark:hover:bg-slate-800"
        onClick={toggleModal}
       >
-        <Cv />
+<CV />
         <p className="">CV</p>
 
       </button>
 
       {/* Modal */}
       {isOpen && (
-        <div className="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-40 p-4">
+        <div className="fixed inset-0 z-10 flex items-center justify-center p-4 bg-black bg-opacity-40">
           <div className="w-full p-6 bg-white rounded-lg shadow-lg max-w-[50rem]">
             <h2 className="mb-4 text-xl font-bold text-gray-950">Elige el idioma</h2>
             <div className="flex justify-between gap-4">
